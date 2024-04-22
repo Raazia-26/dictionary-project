2d24a6180d6643ad50d1df462d7ad37ad3a2ab06
Raazia-26 <razi.raazia@gmail.com>
2024-04-23 01:54:39 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary App</h1>
+        <img src={logo} className="app-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
