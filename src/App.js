2f5c0048e517157fb15cf6389f6ec639abaea17e
Raazia-26 <razi.raazia@gmail.com>
2024-04-24 12:31:46 +0200

    Added the search engine

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary App</h1>
-        <img src={logo} className="app-logo img-fluid" alt="logo" />
-      </header>
-      <a href="#" className="btn btn-primary">
-        Hello
-      </a>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary App</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            {" "}
+            Coded by Raazia Razi and sourced at{" "}
+            <a
+              href="https://github.com/Raazia-26/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
