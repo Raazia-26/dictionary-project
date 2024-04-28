e7019398b687c64400ce81545acee349e0fc1e48
Raazia-26 <razi.raazia@gmail.com>
2024-04-28 02:04:27 +0200

    integrated API for photos and styled it

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,23 +2,35 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import WordDefinition from "./WordDefinition";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [wordData, setwordData] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-    console.log(response);
     setwordData(response.data);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+
+  function handlePhotoResponse(response) {
+    console.log(response);
+    setPhotos(response.data.photos);
+  }
+
   function search(event) {
     event.preventDefault();
     let apiKey = "dfd604f6396ftb7o693bf0062e2334aa";
+    let apiKeyPhotos =
+      "gz6OM6yLvAAh9eDIfvJNTjBYJG1KitLw3fBIeGNayVnUe660rSGz7p8H";
+    let headers = { Authorization: ` ${apiKeyPhotos}` };
+    let pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+    axios.get(pexelApiUrl, { headers: headers }).then(handlePhotoResponse);
   }
 
   return (
@@ -36,6 +48,7 @@ export default function Dictionary() {
         </form>
       </section>
       <WordDefinition data={wordData} />
+      <Photos photos={photos} />
     </div>
   );
 }
